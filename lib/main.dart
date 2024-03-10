76d48c4f481466f6c6e90edff20bd9eeb02422f8
Hendra Kurniawan <hendra.kurniawan88@japfa.com>
2024-03-10 07:31:11 +0700

    Starter Kit

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:intl/date_symbol_data_local.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  // objectbox = await ObjectBox.create();
+  await Supabase.initialize(
+    url: 'https://aueevfggytzboyeszwlw.supabase.co',
+    anonKey:
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF1ZWV2ZmdneXR6Ym95ZXN6d2x3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTAwMjc1MDksImV4cCI6MjAyNTYwMzUwOX0.C4iVOFeeQYhXb8jVGPbYhAqUu6fvD7r1oc4ACvcnzC8',
+  );
+  initializeDateFormatting();
   runApp(const MyApp());
 }
 
@@ -10,12 +20,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demoxxx',
+      title: 'Ramadhan OGP Portal',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Pesantren Kilat OGP'),
     );
   }
 }
